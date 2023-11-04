c093a47cedc2a17172831780cf136190018cfffc
2946242422 <2946242422@qq.com>
2023-11-04 18:37:00 +0800

    OneGame

---
+++ OneGame/assets/Scripts/GameManager.ts
@@ -0,0 +1,25 @@
+import { CCInteger,_decorator, Component, Node ,Prefab} from 'cc';
+
+const { ccclass, property } = _decorator;
+
+enum BlockType{
+    BT_NONE,
+    BT_STONE,
+}
+@ccclass('GameManager')
+export class GameManager extends Component {
+    @property({type:Prefab})
+    public  boxPrefab:Prefab|null=null;
+    @property({type: CCInteger})
+    public roadLength: number = 50;
+    private _road: BlockType[] = [];
+    start() {
+            //...
+    }
+
+    update(deltaTime: number) {
+        
+    }
+}
+
+
